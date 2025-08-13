ea7ca18c3b3425441ecfc90c85c26afc9a2773e9
pushkarjain2215 <108095866+pushkarjain2215@users.noreply.github.com>
2025-08-14 02:04:00 +0530

    Add files via upload

---
+++ Counter-App/index.js
@@ -0,0 +1,30 @@
+let saveEl = document.getElementById("save-el");
+let countEl = document.getElementById("count-el");
+
+let count = 0;
+
+function increment() {
+    count += 1;
+    countEl.textContent = count;
+}
+
+function decrement() {
+    count -= 1;
+    if (count < 1) {
+        count = 0;
+    }
+    countEl.textContent = count;
+}
+
+function save() {
+    let countStr = count + " - ";
+    saveEl.textContent += countStr;
+    countEl.textContent = 0;
+    count = 0;
+}
+
+function reset() {
+    saveEl.textContent = "Previous entries : ";
+    countEl.textContent = 0;
+    count = 0;
+}
